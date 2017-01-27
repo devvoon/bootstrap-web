681df53aca7a73ea7fd051a3dbc80e969df29f97
dawoonjeong <dawoonjeong@hotmail.com>
2017-01-27 22:09:47 +0900

    ver1.0 blog

---
+++ blog/About.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="../../favicon.ico">
+
+        <title>Welcome to DaWoon's World</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="../../bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="../../css/cover.css" rel="stylesheet">
+
+        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+        <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+        <script src="../../bootstrap-3.3.4-dist/js/ie-emulation-modes-warning.js"></script>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="../index.php">DawoonJeong</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li><a href="../index.php">Home</a></li>
+                        <li class="active"><a href="About.php">About</a></li>
+                        <li><a href="Archive.php">Archive</a></li>
+                    </ul>
+                </div><!-- /.nav-collapse -->
+            </div><!-- /.container -->
+        </nav><!-- /.navbar -->
+
+      <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+      <div class="container">
+         <div class="container marketing">
+            <!-- START THE FEATURETTES -->
+            <hr class="featurette-divider">
+            <div class="row featurette">
+              <div class="col-md-12">
+                <h2 class="featurette-heading">About. <span class="text-muted">정다운</span></h2>
+                <p class="lead">정다운을 소개합니다.</p>
+                <img class="featurette-image img-responsive center-block" src="image/about_main.jpg" alt="about_whoami" width="30%" height="30%">
+              </div>
+            </div>
+
+            <hr class="featurette-divider">
+            <div class="row featurette">
+              <div class="col-md-7 col-md-push-5">
+                <h2 class="featurette-heading">#ComingWoon <span class="text-muted">2017~</span></h2>
+                 <p class="lead">?</p>
+              </div>
+              <div class="col-md-5 col-md-pull-7">
+                <img class="featurette-image img-responsive center-block" src="image/about_ComingWoon.jpg"  alt="about_comingwoon" width="60%" height="60%">
+              </div>
+            </div>
+
+            <hr class="featurette-divider">
+            <div class="row featurette">
+              <div class="col-md-7">
+                <h2 class="featurette-heading">dawoonjeong.com <span class="text-muted">2017.01~</span></h2>
+                <p class="lead">개인 홈페이지 오픈</p>
+                <p class="lead">HTML / PHP / Java Script</p>
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-responsive center-block" src="image/about_dawoonjeong.jpg" alt="about_dawoonjeong.com" border="1" width="60%" height="60%">
+              </div>
+            </div>
+
+            <hr class="featurette-divider">
+            <div class="row featurette">
+              <div class="col-md-7 col-md-push-5">
+                <h2 class="featurette-heading">SM <span class="text-muted">2015.11~</span></h2>
+                 <p class="lead">공공의료기간 종합병원 의료시스템 유지보수 및 관리</p>
+                 <p class="lead">PL SQL / XML / C#  / VB script / HTML</p>
+              </div>
+              <div class="col-md-5 col-md-pull-7">
+                <img class="featurette-image img-responsive center-block" src="image/about_EMR.jpg"  alt="about_sm" width="60%" height="60%">
+              </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+              <div class="col-md-7">
+                <h2 class="featurette-heading">SI <span class="text-muted">2014.03~2015.10</span></h2>
+                <p class="lead">공공의료기관 5개 병원의 통합의료 정보시스템 구축 및 오픈</p>
+                <p class="lead">PL SQL / XML / C#  / VB script / HTML</p>
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-responsive center-block" src="image/about_EMR.jpg"  alt="about_si" width="60%" height="60%">
+              </div>
+            </div>
+
+            <hr class="featurette-divider">
+            <div class="row featurette">
+              <div class="col-md-7 col-md-push-5">
+                <h2 class="featurette-heading">실무 Project 기반<br>Enterprise JAVA 전문가 과정<br>
+                    <span class="text-muted">2013.06~2013.12</span></h2>
+                 <p class="leadå">Spring Framework, AOP, MVC, 세션 API, IBatis, 트렌젝션 처리,<BR> Android 등의 프로젝트 진행</p>
+                 <p class="lead">JAVA / jsp / Spring / Android / oracle / XML </p>
+              </div>
+              <div class="col-md-5 col-md-pull-7">
+                <img class="featurette-image img-responsive center-block" src="image/about_kitri.jpg"  alt="about_kitri" width="60%" height="60%">
+              </div>
+            </div>
+
+            <hr class="featurette-divider">
+            <!-- /END THE FEATURETTES -->
+            <!-- FOOTER -->
+            <footer>
+              <p class="pull-right"><a href="#">Back to top</a></p>
+              <p>&copy; 2017 DawoonJeong, Inc. All rights reserved.
+            </footer>
+
+         </div>
+      </div><!-- /.container -->
+
+      <!-- Bootstrap core JavaScript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+      <script src="../../dist/js/bootstrap.min.js"></script>
+      <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+      <script src="../../assets/js/vendor/holder.js"></script>
+      <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+      <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Welcome to DaWoon's World</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../../css/cover.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../bootstrap-3.3.4-dist/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">DawoonJeong</h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active">
+                      <a href="../index.php">Home</a></li>
+                  <li><a href="../blog/About.php">About</a></li>
+                  <li><a href="../blog/Archive.php">Archive</a></li>
+                </ul>
+              </nav>
+            </div>
+        </div>
+          <div class="inner cover">
+            <h1 class="cover-heading">I am DaWoon Jeong</h1>
+            <p class="lead">아름다운 정다운이 만들어가는 개발자 블로그입니다.</p>
+            <!--<p class="lead">
+              <a href="#" class="btn btn-lg btn-default">Learn more</a>
+          </p>-->
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+                <p>&copy; 2017 DawoonJeong, Inc. All rights reserved.
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+
+
+
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../../bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+</body>
+</html>
